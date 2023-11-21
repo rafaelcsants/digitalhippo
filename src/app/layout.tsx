63c9f75e2ac50cc979ec917dfb22ae42098f6640
Rafael Santos <rafaelcsants@hotmail.com>
2023-11-21 15:42:44 +0000

    Landing Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 	return (
 		<html lang="en" className="h-full">
 			<body className={cn("relative h-full font-sans antialiased")}>
-				{children}
+				<main className="relative flex flex-col min-h-screen">
+					<div className="flex-grow flex-1">{children}</div>
+				</main>
 			</body>
 		</html>
 	);
